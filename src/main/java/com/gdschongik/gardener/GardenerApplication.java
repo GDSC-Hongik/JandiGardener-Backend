c0c9b59533bbd46bd7603f4de9f7620c4004fa20
chosw1002@naver.com <chosw1002@naver.com>
2023-05-11 19:24:25 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/gdschongik/gardener/GardenerApplication.java
@@ -0,0 +1,13 @@
+package com.gdschongik.gardener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdschongik/gardener/GardenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdschongik.gardener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
