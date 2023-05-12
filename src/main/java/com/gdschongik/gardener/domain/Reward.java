a6973eb86bd204da99f2d92a425ae3b12c70d0c1
chosw1002@naver.com <chosw1002@naver.com>
2023-05-12 21:21:19 +0900

    feat: 리워드 엔티티 생성

---
+++ src/main/java/com/gdschongik/gardener/domain/Reward.java
@@ -0,0 +1,41 @@
+package com.gdschongik.gardener.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Reward {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    private String description;
+    private String redeem;
+    @CreationTimestamp
+    private LocalDateTime receivedAt;
+
+    @Builder
+    public Reward(User user, String description, String redeem, LocalDateTime receivedAt) {
+        this.user = user;
+        this.description = description;
+        this.redeem = redeem;
+        this.receivedAt = receivedAt;
+    }
+    public static Reward createReward(User user, String description, String redeem, LocalDateTime receivedAt) {
+        return Reward.builder()
+                .user(user)
+                .description(description)
+                .redeem(redeem)
+                .receivedAt(receivedAt)
+                .build();
+    }
+}
