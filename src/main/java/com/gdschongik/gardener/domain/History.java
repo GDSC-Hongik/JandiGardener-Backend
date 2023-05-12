f53f421bb7eb304febeee67fa149dd6cfeb5505b
chosw1002@naver.com <chosw1002@naver.com>
2023-05-12 21:14:16 +0900

    feat: 히스토리 엔티티 생성

---
+++ src/main/java/com/gdschongik/gardener/domain/History.java
@@ -0,0 +1,37 @@
+package com.gdschongik.gardener.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Getter
+@NoArgsConstructor
+@Entity
+public class History {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+
+    private int commitCount;
+    private LocalDateTime date;
+
+    @Builder
+    public History(User user, int commitCount, LocalDateTime date) {
+        this.user = user;
+        this.commitCount = commitCount;
+        this.date = date;
+    }
+
+    public static History createHistory(User user, int commitCount, LocalDateTime date){
+        return History.builder()
+                .user(user)
+                .commitCount(commitCount)
+                .date(date)
+                .build();
+    }
+}
