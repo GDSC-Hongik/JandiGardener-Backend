273cb8bf9a763942f97e1ab289e43dfb4409f4c1
uwoobeat <desktop3711@gmail.com>
2023-05-14 23:37:53 +0900

    feat: 챌린지 컨트롤러 생성

---
+++ src/main/java/com/gdschongik/gardener/controller/ChallengeController.java
@@ -0,0 +1,22 @@
+package com.gdschongik.gardener.controller;
+
+import com.gdschongik.gardener.dto.challenge.ChallengeRegisterResponse;
+import com.gdschongik.gardener.service.ChallengeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("v1/challenges")
+public class ChallengeController {
+    private final ChallengeService challengeService;
+
+    public ChallengeController(ChallengeService challengeService) {
+        this.challengeService = challengeService;
+    }
+
+}
