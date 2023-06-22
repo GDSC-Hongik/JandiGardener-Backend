11a2a4de758d030d7fa2da72d3411632b0f43ac2
uwoobeat <desktop3711@gmail.com>
2023-06-22 19:28:42 +0900

    feat: oauth2 인증 id, refresh token으로 유저 찾기 추가

--- src/main/java/com/gdschongik/gardener/repository/UserRepository.java
+++ src/main/java/com/gdschongik/gardener/repository/UserRepository.java
@@ -4,6 +4,11 @@ import com.gdschongik.gardener.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByRefreshToken(String refreshToken);
+
+    Optional<User> findBySocialId(String socialId);
 }
